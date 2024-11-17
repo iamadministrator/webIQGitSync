8bc384aae4b71bde91e16882bdf0a3ba5f193213
iamadministrator <aws.administrator+infrastructure@webiq.cloud>
2024-11-17 14:50:11 -0500

    GitSync Third Attempt

--- build.sh
+++
@@ -1,4 +0,0 @@
-aws cloudformation create-stack --stack-name iOn \
-   --template-body file://iOn.yml \
-   --capabilities CAPABILITY_NAMED_IAM \
-   --region us-east-1 --profile iam.administrator-management
